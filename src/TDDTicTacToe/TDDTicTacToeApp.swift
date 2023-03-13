1c3fc4bece607a45e634fd8babe992e176c9cc5f
aB <self.alexb@gmail.com>
2023-03-13 13:46:44 -0500

    initial commit

---
+++ src/TDDTicTacToe/TDDTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDTicTacToeApp.swift
+//  TDDTicTacToe
+//
+//  Created by aB on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TDDTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
